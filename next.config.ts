9baef1ead8d7379814d8c583f86327d78007de07
houdaifa bousbai <106620881+houdahmed@users.noreply.github.com>
2025-12-23 22:59:18 +0100

    implement api routes, server actions and caching, and the event details page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://eu-assets.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://eu.i.posthog.com/:path*",
+      },
+    ];
+  },
+
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
