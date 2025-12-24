c358845b90100a48ba7b784b6062ef8b3c5b5044
houdaifa bousbai <106620881+houdahmed@users.noreply.github.com>
2025-12-24 23:15:39 +0100

    comply with v16 gods

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,15 @@ import EvenetCard from "@/components/EvenetCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
+import events from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 const page = async () => {
   "use cache";
   cacheLife("hours");
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+  //const response = await fetch(`${BASE_URL}/api/events`);
+  //const { events } = await response.json();
   return (
     <section>
       <h1 className="text-center">
