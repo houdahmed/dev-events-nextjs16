cf38a725491d91a7cd56a026efed3d03579dab54
houdaifa bousbai <106620881+houdahmed@users.noreply.github.com>
2025-12-24 22:36:26 +0100

    finalize the app

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,11 @@ import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
 import { cacheLife } from "next/cache";
 
-
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 const page = async () => {
-  'use cache';
-  cacheLife('hours')
+  "use cache";
+  cacheLife("hours");
   const response = await fetch(`${BASE_URL}/api/events`);
   const { events } = await response.json();
   return (
